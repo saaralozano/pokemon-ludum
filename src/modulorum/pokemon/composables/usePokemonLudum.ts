140684fb5c3e07908305023060480893a8f9f172
saaralozano <saloz2001@gmail.com>
2025-03-12 18:21:16 +0100

    Opciones con botones y confeti

--- src/modulorum/pokemon/composables/usePokemonLudum.ts
+++ src/modulorum/pokemon/composables/usePokemonLudum.ts
@@ -1,6 +1,7 @@
 import { computed, onMounted, ref } from "vue"
 import { LudumStatus, type Pokemon, type PokemonListaResponsio } from "../interfaces"
 import { pokemonApi } from "@/api/pokemonApi";
+import confetti from "canvas-confetti";
 
 export const usePokemonLudum = () => {
     const ludumStatus = ref<LudumStatus>( LudumStatus.Ludit );
@@ -40,6 +41,24 @@ export const usePokemonLudum = () => {
         pokemons.value = pokemons.value.slice(quot);
     }
 
+    const examineResponsio = (id: number) => {
+        const vicit = temerePokemon.value.id === id;
+
+        if (vicit) {
+            ludumStatus.value = LudumStatus.Vicit;
+
+            confetti({
+                particleCount: 300,
+                spread: 150,
+                origin: {y: 0.6}
+            });
+
+            return;
+        }
+
+        ludumStatus.value = LudumStatus.Perdidit;
+    }
+
     onMounted(async() => {
         await new Promise((r) => setTimeout(r, 500));
         pokemons.value = await obtinePokemons();
@@ -56,6 +75,6 @@ export const usePokemonLudum = () => {
         pokemonOptiones,
         sequentiOptiones,
         temerePokemon,
-
+        examineResponsio,
     }
 }
